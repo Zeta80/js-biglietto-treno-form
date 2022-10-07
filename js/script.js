9e90ca3d14396aee6b2b9dd7fb723de08b063da2
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-07 17:28:56 +0200

    need sales

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ const userTicket = document.getElementById("user-ticket-cost");
 console.log(nameUser, userOfferts, userCarriage, userCp, userTicket);
 
 // SUBMIT FUCTION
+
+submitBtn.addEventListener("click", function () {
+    //al click prendo le informazioni
+    const userName = userNameInput.value;
+    console.log(userName);
+
+    //inserisco il valore nello span
+
+    nameUser.innerHTML = 
+
+})
