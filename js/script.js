6a6c50ada1bcfbfa7dc350f99dfeed60504f1dd9
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-07 14:49:55 +0200

    add variabiles

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const userNameInput = document.getElementById("user-name");
+const userDistanceInput = document.getElementById("user-distance");
+const submitBtn = document.getElementById("submit");
+const clearBtn = document.getElementById("clear");
+console.log(userNameInput, userDistanceInput, submitBtn, clearBtn);
+
+// VARIABILI USER
+const nameUser = document.getElementById("name-user");
+const userOfferts = document.getElementById("user-offerts");
+const userCarriage = document.getElementById("user-carriage");
+const userCp = document.getElementById("user-codecp");
+const userTicket = document.getElementById("user-ticket-cost");
+console.log(nameUser, userOfferts, userCarriage, userCp, userTicket);
+
+// SUBMIT FUCTION
